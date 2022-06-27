c45cf5549056321a059dc8092b294d48596a700b
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-06-27 15:16:06 +0400

    Update firebaseConfig.js

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -3,14 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAjQ4CjkX7KlHAwHnE35IBfghNW0BhE9hI",
-    authDomain: "fir-50899.firebaseapp.com",
-    projectId: "fir-50899",
-    storageBucket: "fir-50899.appspot.com",
-    messagingSenderId: "793649616349",
-    appId: "1:793649616349:web:4a06055ec303db81d0d234",
-    measurementId: "G-RMNWF9MXBR"
+const firebaseConfig = {// use your firebase configg
 };
 
 
